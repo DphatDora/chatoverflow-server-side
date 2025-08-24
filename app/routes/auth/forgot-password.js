3d1a2804caf3b2ac289992213eaedb6ce5a3a1f2
raichuvn11 <raichuvn11@gmail.com>
2025-08-24 13:47:41 +0700

    forgot password feature

--- app/routes/auth/forgot-password.js
+++ app/routes/auth/forgot-password.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const forgotPasswordController = require('../../controller/auth/ForgotPassword.controller');
 
 router.get('/', (req, res) => {
    // handle forgot password logic here, pleaseeeeeee
    res.send('Forgot Password route');
 });
+router.post('/send-otp', forgotPasswordController.sendOTP); // {"email": "user@example.com"}
+router.post('/verify-otp', forgotPasswordController.verifyOTP); // {"email": "user@example.com", "otp": "123456"}
+router.post('/reset-password', forgotPasswordController.resetPassword); // {"resetToken": "token", "newPassword": "newPassword"}
 
 module.exports = router;
