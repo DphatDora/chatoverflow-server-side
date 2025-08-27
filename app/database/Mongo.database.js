a5181e50122480a6f97afdb35d932aa0ae1578de
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-08-27 20:44:51 +0700

    fix: restructure mongo connection logic to fix some failure

--- app/database/Mongo.database.js
+++ app/database/Mongo.database.js
@@ -1,15 +1,19 @@
+// Note: In production, the timing is strick so if we try to access mongoose.connection.db.databaseName before it's connected,
+// it will cause the app to crash
+
 const mongoose = require('mongoose');
 
 const dbConf = require('../config/db.conf');
 
 async function connectMongo() {
    try {
-      await mongoose.connect(dbConf.uri, dbConf.options);
+      const connection = await mongoose.connect(dbConf.uri, dbConf.options);
       console.log('MongoDB successfully connected');
-      console.log(
-         'Databse name: ',
-         mongoose.connection.db.databaseName
-      );
+      // console.log('Databse name: ', mongoose.connection.db.databaseName);
+
+      // instead of call the database name by mongoose which potentially failure if connect haven't established
+      // we can call it by an established connection
+      console.log('Databse name: ', connection.connection.db.databaseName);
    } catch (err) {
       console.error('MongoDB connection failure:', err.message);
       process.exit(1);
@@ -17,3 +21,6 @@ async function connectMongo() {
 }
 
 module.exports = connectMongo;
+
+// But who the hell write the previous dirty code ?
+// Aha, it's me...
