71b12f49c80d4faccc72a81c86e2a72c6a70da75
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-08-27 22:58:59 +0700

    feat: adjust cors so that no orgin request can also approved

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ const allowedOrigins = [
 app.use(
    cors({
       origin: function (origin, callback) {
+         // allow requests with no origin postman
+         if (!origin) return callback(null, true);
          if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
          } else {
