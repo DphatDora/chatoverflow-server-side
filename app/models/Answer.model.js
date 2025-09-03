160e27d97a12fbe3a1bccc1b990791fabd00f435
raichuvn11 <raichuvn11@gmail.com>
2025-09-03 15:25:29 +0700

    add api load question by newest | trending | unanswer

---
+++ app/models/Answer.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const AnswerSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    question: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    upvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    downvotedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Answer", AnswerSchema);
