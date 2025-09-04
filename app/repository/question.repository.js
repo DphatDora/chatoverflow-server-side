397391d1b511042d2fde1b68fd8010cc28355ddd
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2025-09-04 16:30:31 +0700

    chore: add prettier config file

--- app/repository/question.repository.js
+++ app/repository/question.repository.js
@@ -2,81 +2,81 @@ const Question = require('../models/Question.model');
 const Answer = require('../models/Answer.model');
 
 async function getNewest(limit = 20) {
-   return Question.find()
-      .sort({ createdAt: -1 })
-      .limit(limit)
-      .populate('user', 'name avatar')
-      .populate('answerCount');
+  return Question.find()
+    .sort({ createdAt: -1 })
+    .limit(limit)
+    .populate('user', 'name avatar')
+    .populate('answerCount');
 }
 
 async function getTrending(limit = 20) {
-   return Question.aggregate([
-      {
-         $addFields: {
-            upvoteCount: { $size: '$upvotedBy' },
-         },
+  return Question.aggregate([
+    {
+      $addFields: {
+        upvoteCount: { $size: '$upvotedBy' },
       },
-      {
-         $lookup: {
-            from: 'answers',
-            localField: '_id',
-            foreignField: 'question',
-            as: 'answers',
-         },
+    },
+    {
+      $lookup: {
+        from: 'answers',
+        localField: '_id',
+        foreignField: 'question',
+        as: 'answers',
       },
-      {
-         $addFields: {
-            answerCount: { $size: '$answers' },
-         },
+    },
+    {
+      $addFields: {
+        answerCount: { $size: '$answers' },
       },
-      { $sort: { views: -1, upvoteCount: -1 } },
-      { $limit: limit },
-      {
-         $project: {
-            answers: 0,
-         },
+    },
+    { $sort: { views: -1, upvoteCount: -1 } },
+    { $limit: limit },
+    {
+      $project: {
+        answers: 0,
       },
-   ]);
+    },
+  ]);
 }
 
 async function getUnanswered(limit = 20) {
-   return Question.aggregate([
-      {
-         $lookup: {
-            from: 'answers',
-            localField: '_id',
-            foreignField: 'question',
-            as: 'answers',
-         },
+  return Question.aggregate([
+    {
+      $lookup: {
+        from: 'answers',
+        localField: '_id',
+        foreignField: 'question',
+        as: 'answers',
       },
-      { $match: { 'answers.0': { $exists: false } } },
-      { $sort: { createdAt: -1 } },
-      { $limit: limit },
-      {
-         $addFields: { answerCount: 0 },
-      },
-   ]);
+    },
+    { $match: { 'answers.0': { $exists: false } } },
+    { $sort: { createdAt: -1 } },
+    { $limit: limit },
+    {
+      $addFields: { answerCount: 0 },
+    },
+  ]);
 }
 
 async function getQuestionDetailById(id) {
-   try {
-      const question = await Question.findById(id)
-         .populate('user', 'name avatar')
-         .populate({
-            path: 'answerCount',
-         })
-         .exec();
+  try {
+    const question = await Question.findById(id)
+      .populate('user', 'name avatar')
+      .populate({
+        path: 'answerCount',
+      })
+      .exec();
 
-      return question;
-   } catch (err) {
-      console.log('Failure raised at question.repository', err);
-      return null;
-   }
+    return question;
+  } catch (err) {
+    console.log('Failure raised at question.repository', err);
+    return null;
+  }
 }
 
 module.exports = {
-   getNewest,
-   getTrending,
-   getUnanswered,
-   getQuestionDetailById,
+  getNewest,
+  getTrending,
+  getUnanswered,
+  getQuestionDetailById,
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-   res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 router.use('/auth', require('./auth'));
 router.use('/user', require('./user'));
